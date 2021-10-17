48fc6f71518bcbc659978c855516e1b4141d4016
ROBERT DE JESUS GENAO MOLINA <1071499@est.intec.edu.do>
2021-10-17 09:11:18 +0800

    Add project files.

---
+++ SoundOff/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SoundOff.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Sound Off";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ SoundOff/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SoundOff.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SoundOff.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
